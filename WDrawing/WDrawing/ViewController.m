050dc225effe845bbc1ba93087a9c21af9e679cb
minggangwang_vendor <minggang.wang@topsales.cc>
2016-08-30 17:04:58 +0800

    creat project

---
+++ WDrawing/WDrawing/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WDrawing
+//
+//  Created by apple on 16/8/30.
+//  Copyright © 2016年 W.Miga. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
